cbbd7fc06f32c64ed288b5ed54630b5f212548de
Orestas Dulinskas <orestasdulinskas@gmail.com>
2022-02-08 15:03:39 +0000

    Opening project

---
+++ src/test/java/com/example/tsi/orestas/dulinskas/demo/SakilaDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tsi.orestas.dulinskas.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SakilaDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
