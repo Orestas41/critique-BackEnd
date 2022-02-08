efaa18ae14e86e5a70000209c42fa2d9070b5ead
Orestas Dulinskas <orestasdulinskas@gmail.com>
2022-02-08 17:24:26 +0000

    Added language repository

--- src/main/java/com/example/tsi/orestas/dulinskas/demo/SakilaDatabaseApplication.java
+++ src/main/java/com/example/tsi/orestas/dulinskas/demo/SakilaDatabaseApplication.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/HomePage")
 public class SakilaDatabaseApplication {
 
+
 	@Autowired
 	private LanguageRepository languageRepository;
 
